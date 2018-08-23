6d9c621bc35d4f3ede02aa4d8ead482f5ab0b070
Kasem <reza110786@gmail.com>
2018-08-23 20:17:22 +0530

    Initial commit

---
+++ app/src/main/java/lib/kasem/knifecutter/MainActivity.java
@@ -0,0 +1,15 @@
+package lib.kasem.knifecutter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lib.kasem.kasemknifecutter.KasemKnifeCutter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ kasemknifecutter/src/main/java/lib/kasem/kasemknifecutter/KasemKnifeCutter.java
@@ -0,0 +1,31 @@
+package lib.kasem.kasemknifecutter;
+
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class KasemKnifeCutter  {
+    private static int calculateSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+        if (height > reqHeight || width > reqWidth) {
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+            while ((halfHeight / inSampleSize) >= reqHeight &&
+                    (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *=2;
+            }
+        }
+        return inSampleSize;
+    }
+    public static Bitmap decodeSampleBitmap(Resources res, int resId, int reqWidth, int reqHeight) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+        options.inSampleSize = calculateSize(options, reqWidth, reqHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+}
